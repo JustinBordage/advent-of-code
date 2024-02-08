f36af5f59b689cd231791a90684dfe9a755b849f
--system <38659460+JustinBordage@users.noreply.github.com>
2024-02-08 15:16:16 -0400

    Refactored the evalPerformance to return the average instead of the total time.

--- src/common/performance.ts
+++ src/common/performance.ts
@@ -11,7 +11,8 @@ function run(fn: Function, count: number, ...args: any[]) {
 export function evalPerformance<T extends Function>(fnList: T[], ...args: any[]) {
 	[10, 100, 1000, 10000, 100000].forEach(count => {
 		fnList.forEach(fn => {
-			console.log(fn.name, count, `${run(fn, count, ...args).toFixed(5)}ms`);
+			const elapsedTime = run(fn, count, ...args);
+			console.log(fn.name, `Avg: ${(elapsedTime / count).toFixed(5)}ms | ${count} Cycles`);
 		});
 	});
 }
